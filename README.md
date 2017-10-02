bf5c7b78c255b7e864c5434e230fc8f857a459a9
Robert Sweetman <robertiansweetman@gmail.com>
2017-10-02 23:52:11 +0100

    add example and screenshot

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-app-personal
+# Powershell Invoke-RestMethod (debugging)
+
+I've used Invoke-RestMethod for publishing QA report data to Slack using their WebHooks and also to pre-load data into an application via RestAPI calls.
+
+One thing that's 'not great' about vanilla Invoke-RestMethod is there's sometimes a lack of feedback if things don't work 100% and it can be a challenge to understand why something's gone bang...
+
+So here's a simple example. In this case I went with using -ErrorVariable because I felt it was a bit clearer as to what's happening although there's also a 'try/catch' example in the comments. I should also probably try this with Invoke-WebRequest and see what (if any) differences there are.
+
+In this example I was also struggling with the fact that the Rest-API that I was testing against doesn't _seem_ to return a 200 Success code when things work properly. Calling the function with added -Verbose flag got me the response from the $(headerToken) call but not the 'success' which was weird. I'll have to get into that with the development team 'cause I'm actually using our API/application to test against!  
+
+Here's what you get in addition to angry red noise:
+
+![alt text](https:\\pathtoPost-method.png)
---
+++ invokeRestDebugging.ps1
@@ -0,0 +1,75 @@
+<# NOTE: the Invoke-RestMethod call below relies on there being a headerToken function which has already been loaded!
+
+    function headerToken {
+        In my example the security credentials for accessing the API/backend are created here
+        read the Invoke-RestMethod documentation for other options/replacements here
+    }
+#>
+
+<# NOTE: you need a 'body' which is usually in this format
+
+    $body = @"
+        {
+            "Key with int value":0,
+            "Key with string value": "some string",
+            "Key with boolean value": false,
+            "Key with null value": null,
+            "Last key has no comma": "more string!"
+        }
+"@
+
+#>
+
+function POST-Method {
+
+    [Cmdletbinding()]
+        param(
+            [parameter()]
+            [string]$uri,
+            [string]$body
+        )
+
+    #  
+
+    <# NOTE: this is a basic alternative option using Try/catch
+    
+        try {
+            Invoke-RestMethod -Uri $uri -Header $(headerToken) -Method POST -Body $body -ContentType "application/json" 
+        } catch {
+            Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+            Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+        }
+    
+    #>
+
+    $POSTcall = Invoke-RestMethod -Uri $Uri -Headers $(headerToken) -Body $Body -Method POST -ContentType "application/json" -ErrorVariable Issue -Timeout 3
+    
+    # catches any error data from $Error[0] which is the last failure recieved by calling Invoke-RestMethod
+    # In this case try/catch is probably better but I wanted more information  
+    if ($Issue -ne $null) {
+    
+        Write-Host "StatusCode:"$Error[0].Exception.Response.StatusCode.value__
+        Write-Host "StatusDescription:" $Error[0].Exception.Response.StatusCode
+        Write-Host "ErrorCausedBy:" $Error[0].InvocationInfo.Line
+        Write-Host "Exception:" $Error[0].Exception
+    
+    # I'm trying to debug my own stuff to see when/if? something is returned I can see it okay    
+    } elseif ($POSTcall -ne $null) {
+    
+        $POSTcall | ConvertFrom-Json
+        Write-Host $POSTcall
+    
+    # Added -Timeout and 'return' below to try to get out of this function 
+    # In my case calling it goes on forever since (as I mentioned) I don't seem to get a 200 Success response
+    # Timeout does allow the program to continue in this case although it incorrectly returns Failure(timeout) when in actual fact the API call succeeds... Not very satisfactory!
+    
+    } else {
+    
+        return
+        
+    }
+    
+}
+}
+
+POST-Method -Uri "https://example" -Body $body -Verbose
